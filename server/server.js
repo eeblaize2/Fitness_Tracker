d85589abd26a20240572627139f05a46f750aea3
Erin Blaize <eeblaize2@hotmail.com>
2021-07-07 18:44:09 -0700

    changed to process.env

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT||3000
 const connect_db= require('./connect.js')
 const dotenv= require('dotenv')
 const path= require('path')
